f1af2b5814fcf7cfd6f094703eaaafb476c80b98
bharathmighty248 <bharathmighty248@gmail.com>
2021-09-17 19:10:51 +0530

    [Bharath]Add:Contact Class To Store Contacts In Address Book

---
+++ AddressBookSystem.js
@@ -0,0 +1,46 @@
+console.log("AddressBook System");
+class Contact {
+
+    constructor(...params) {
+      this.firstName = params[0];
+      this.lastName = params[1];
+      this.address = params[2];
+      this.city = params[3];
+      this.state = params[4];
+      this.zip = params[5];
+      this.phoneNo = params[6];
+      this.email = params[7];
+    }
+
+    get firstName() {return this._firstName;}
+    set firstName(firstName) { this._firstName = firstName;}
+    
+    get lastName() {return this._lastName;}
+    set lastName(lastName) {this._lastName = lastName;}
+    
+    get address() {return this._address;}
+    set address(address) {this._address = address; }
+    
+    get city() {return this._city;}
+    set city(city) {this._city = city;}
+    
+    get state() {return this._state;}
+    set state(state) {this._state = state;}
+    
+    get zip() {return this._zip;}
+    set zip(zip) {this._zip = zip;}
+    
+    get phoneNo() {return this._phoneNo;}
+    set phoneNo(phoneNo) {this._phoneNo = phoneNo;}
+    
+    get email() {return this._email;}
+    set email(email) {this._email = email;}
+
+    toString(){
+        return "First Name : "+ this.firstName + "\nLast Name : "+ this.lastName + "\nAddress : "+ this.address + 
+        "\nCity : "+ this.city + "\nState : "+ this.state +"\nZip : "+ this.zip+ "\nPhone No : "+ this.phoneNo + "\nEmail : "+ this.email;
+    }
+}  
+
+let contact = new Contact("Guru", "V", "T-Nagar", "Chennai", "TN", "600069", "9876543219", "Guruv@gmail.com");
+console.log(contact.toString());
